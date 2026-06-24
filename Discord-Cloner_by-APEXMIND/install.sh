4290d7fbaed26431340bdb5a7ed182ab2da5f8d6
M1TAUT <m1taut@mail.ru>
2026-06-24 17:39:59 +0300

    Delete Discord-Cloner_by-APEXMIND directory

--- Discord-Cloner_by-APEXMIND/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BREAK_SYS=""
-pip install --help 2>/dev/null | grep -q break-system-packages && BREAK_SYS="--break-system-packages"
-
-pip install $BREAK_SYS discord.py-self colorama audioop-lts
